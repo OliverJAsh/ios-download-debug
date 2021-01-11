88f87594f31b3a78226df9d37eeb724b53d17fa7
Oliver Joseph Ash <oliverjash@gmail.com>
2021-01-11 19:38:30 +0000

    HTTP bin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 app.get("/download", (req, res) => {
   res.redirect(
-    "https://images.unsplash.com/photo-1610372976107-95b5168db00f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=ramiz-dedakovic-ny29rJDM3MY-unsplash.jpg"
+    "https://httpbin.org/response-headers?Content-Disposition=attachment;%20filename%3d%22test.json%22"
   );
 });
 app.listen(process.env.PORT, () => console.log("running"));
