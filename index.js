5c58ab6e6b8a62afe88679eb56c2031c423b0790
Oliver Joseph Ash <oliverjash@gmail.com>
2021-01-11 19:42:47 +0000

    img

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send(
     `
+    <style>
+    body { color: white; background-color: black; }
+    a { color: inherit; }
+    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script>
       window.now = () => {
@@ -12,10 +16,15 @@ app.get("/", (req, res) => {
           oReq.addEventListener("load", () => {});
           oReq.open("GET", "/");
           oReq.send();
+
+          const img = document.createElement('img');
+          img.src = 'https://images.unsplash.com/file-1606243029859-9e03fb70d982image';
+          document.body.appendChild(img);
         }, 10)
       }
     </script>
     <a href="/file" download onclick="window.now()">download</a>
+    <!-- <img src="https://images.unsplash.com/file-1606243029859-9e03fb70d982image" /> -->
   `
   );
 });
